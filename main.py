fa2461954b23e1fcd3f6539c29b5075066b85148
therealvibesggs <therealvibesggs@gmail.com>
2025-05-10 13:01:10 -0400

    updated asteroid to moving around

--- main.py
+++ main.py
@@ -2,18 +2,29 @@
 
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    new_player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
+    fps = 60
+    dt = 0
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        pygame.Surface.fill(screen, (0,0,0))
+        new_player.update(dt)
+
+        screen.fill("black")
+        new_player.draw(screen)
         pygame.display.flip()
+
+        dt = clock.tick(fps) / 1000
 
 
 if __name__ == "__main__":
