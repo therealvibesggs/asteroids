4d8f0a3f451811568b4aa01e929e3bea62bf3310
therealvibesggs <therealvibesggs@gmail.com>
2025-05-13 17:30:35 -0400

    finished collision piece

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 # imports open source pygame library
 
 import pygame
+import sys
+
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from circleshape import CircleShape
 
 def main():
     pygame.init()
@@ -12,7 +17,13 @@ def main():
     update_group = pygame.sprite.Group()
     draw_group = pygame.sprite.Group()
     Player.containers = (update_group, draw_group)
-        
+    
+    asteroid_group = pygame.sprite.Group()
+    Asteroid.containers = (asteroid_group, update_group, draw_group)
+    
+    AsteroidField.containers = (update_group)
+    asteroid_field = AsteroidField()
+
     new_player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
     fps = 60
     dt = 0
@@ -25,12 +36,17 @@ def main():
         # refresh player based on movement 
         update_group.update(dt)
 
+        for asteroid in asteroid_group:
+            if asteroid.collision_check(new_player) == True:
+                sys.exit("Game over!")
+
         # draw screen and draw player on screen
         screen.fill("black")
         for item in draw_group:
             item.draw(screen)
         pygame.display.flip()
 
+        # limit the framewrate to 60 FPS
         dt = clock.tick(fps) / 1000
 
 
